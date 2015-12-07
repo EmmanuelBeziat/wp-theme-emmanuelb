d91b27bc21572ec1daa48954d7b2f04d22da34b5
Emmanuel Béziat <contact@emmanuelbeziat.com>
2015-12-07 22:26:27 +0100

    improve gulp + stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,24 @@
 var gulp = require('gulp'),
-	util = require('gulp-util'),
-	stylus = require('gulp-stylus'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	coffee = require('gulp-coffee'),
-	plumber = require('gulp-plumber'),
-	imagemin = require('gulp-imagemin'),
-	sourcemaps = require('gulp-sourcemaps'),
-	autoprefixer = require('gulp-autoprefixer');
+	plugins = require('gulp-load-plugins')();
 
-var path = {
-	prod: {
-		root: './',
-		css: './css',
-		js: './js',
-		img: './images'
-	},
-	dev: {
-		root: './dev/',
-		stylus: './dev/stylus',
-		coffee: './dev/coffeescript',
-		maps: './dev/maps',
-		img: './dev/images',
-		js: './dev/javascript'
-	}
-};
+var themeName = 'emmanuelb',
+	path = {
+		prod: {
+			root: './',
+			css: './css',
+			js: './js',
+			img: './images',
+			maps: './maps'
+		},
+		dev: {
+			root: './dev/',
+			stylus: './dev/stylus',
+			coffee: './dev/coffeescript',
+			maps: './dev/maps',
+			img: './dev/images',
+			js: './dev/javascript'
+		}
+	};
 
 /**
  * Sauvegarde et compresse les fichiers stylus dans un unique fichier .css
@@ -34,18 +27,18 @@ var path = {
  **/
 gulp.task('stylus', function() {
 	return gulp.src(path.dev.stylus + '/main.styl')
-		.pipe(plumber())
-			.pipe(sourcemaps.init())
-				.pipe(stylus({
+		.pipe(plugins.plumber())
+			.pipe(plugins.sourcemaps.init())
+				.pipe(plugins.stylus({
 					compress: true
 				}))
-				.pipe(autoprefixer({
+				.pipe(plugins.autoprefixer({
 					browsers: ['last 3 versions'],
 					cascade: false
 				}))
-				.pipe(rename('style.css'))
-			.pipe(sourcemaps.write(path.dev.maps))
-		.pipe(plumber.stop())
+				.pipe(plugins.rename('style.css'))
+			.pipe(plugins.sourcemaps.write(path.prod.maps))
+		.pipe(plugins.plumber.stop())
 		.pipe(gulp.dest(path.prod.root));
 });
 
@@ -54,11 +47,11 @@ gulp.task('stylus', function() {
  **/
 gulp.task('coffeescript', function() {
 	return gulp.src(path.dev.coffee + '/*.coffee')
-		.pipe(plumber())
-			.pipe(coffee({
+		.pipe(plugins.plumber())
+			.pipe(plugins.coffee({
 				bare: true
-			}).on('error', util.log))
-		.pipe(plumber.stop())
+			}).on('error', plugins.util.log))
+		.pipe(plugins.plumber.stop())
 		.pipe(gulp.dest(path.dev.js));
 });
 
@@ -68,14 +61,14 @@ gulp.task('coffeescript', function() {
  **/
 gulp.task('javascript', function() {
 	return gulp.src([path.dev.js + '/vendors/*.js', path.dev.js + '/plugins/*.js', path.dev.js + '/*.js'])
-		.pipe(plumber())
-			.pipe(sourcemaps.init())
-				.pipe(uglify({
+		.pipe(plugins.plumber())
+			.pipe(plugins.sourcemaps.init())
+				.pipe(plugins.uglify({
 					preserveComments: 'some'
 				}))
-				.pipe(concat('main.min.js'))
-			.pipe(sourcemaps.write('../dev/maps'))
-		.pipe(plumber.stop())
+				.pipe(plugins.concat('main.min.js'))
+			.pipe(plugins.sourcemaps.write('../maps'))
+		.pipe(plugins.plumber.stop())
 		.pipe(gulp.dest(path.prod.js));
 });
 
@@ -84,7 +77,7 @@ gulp.task('javascript', function() {
  **/
 gulp.task('images', function() {
 	return gulp.src(path.dev.img + '/**/*')
-		.pipe(imagemin())
+		.pipe(plugins.imagemin())
 		.pipe(gulp.dest(path.prod.img));
 });
 
